033cc16aed6133dedb96019fca28354faaf0092b
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-07-11 11:32:40 +0300

    Релизован debounce

--- js/debounce.js
+++ js/debounce.js
@@ -1,9 +1,21 @@
 'use strict';
 
 (function () {
-  window.debounce = function (cb) {
-    return cb;
+  // define timeout
+  var DEBOUNCE_INTERVAL = 500; // 0.5 sec
 
+  window.debounce = function (cb) {
+    // define empty variable in closure
+    var lastTimeout;
+    // return function
+    return function () {
+      // if lastTimeout exists, clear previous timeout
+      if (lastTimeout) {
+        clearTimeout(lastTimeout);
+      }
+      // set timeout to call back
+      lastTimeout = setTimeout(cb, DEBOUNCE_INTERVAL);
+    };
 
   };
 })();
